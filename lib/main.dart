e1e10ffdba85aedf11e43322354175834730ca63
aly-zanaty <166063618+aly-zanaty@users.noreply.github.com>
2024-04-25 14:59:42 +0800

    something
    somethingSomething

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Education App',
+      title: 'SMART HUB',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         fontFamily: 'Poppins',
